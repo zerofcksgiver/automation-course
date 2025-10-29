833a4b8c7eb2fc96383d720992e385c542ff732f
Mark <zerofcksgiver@gmail.com>
2025-10-29 04:02:40 +0200

    added lesson2 play.js

---
+++ lesson2 play.js
@@ -0,0 +1,20 @@
+/* let a = 10;
+let b = a * 2;
+console.log(b); */
+
+/* let b = 42;
+let result = (b * 2) + 5;
+console.log(result); */
+
+// console.log('Hello world!'.length);
+// console.log('Hello'.toUpperCase());
+
+let text;
+text = 'Hello world!';
+let username = 'echo';
+let textCopy = text;
+text = 'Hello JS!';
+
+console.log(text);
+console.log(textCopy);
+console.log(username);
