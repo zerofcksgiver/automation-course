fcb7d6c1feb6f0e6264de88d73c6b386e8b52745
Mark <zerofcksgiver@gmail.com>
2025-10-30 14:09:19 +0200

    update 1

--- lesson2 play.js
+++ lesson2 play.js
@@ -9,7 +9,7 @@ console.log(result); */
 // console.log('Hello world!'.length);
 // console.log('Hello'.toUpperCase());
 
-let text;
+/* let text;
 text = 'Hello world!';
 let username = 'echo';
 let textCopy = text;
@@ -17,4 +17,18 @@ text = 'Hello JS!';
 
 console.log(text);
 console.log(textCopy);
-console.log(username);
+console.log(username); */
+
+/* let str1 = "Hello";
+let str2 = 'World';
+let str3 = `Hello, ${str2}!`;
+console.log(str3); */
+
+/* let username = "echo";
+console.log(`Hello, ${username}!`); */
+
+/* let message = `Это многострочная 
+строка в JavaScript`;
+console.log(message); */
+
+
