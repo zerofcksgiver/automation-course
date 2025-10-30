2bbd00816a304b03564ada0aef6e7365ea6e1d30
Mark <zerofcksgiver@gmail.com>
2025-10-30 18:02:03 +0200

    lesson5 homework started

---
+++ lesson5/task1.js
@@ -0,0 +1,23 @@
+/* Створіть функцію, яка приймає два параметри: width і height та повертає площу прямокутника.
+Реалізуйте функцію трьома способами:
+Function Declaration
+Function Expression
+Arrow Function
+Викличте кожну функцію та виведіть результат у консоль. */
+
+
+//Function Declaration
+
+/* let width = 40;
+let height = 60;
+function multiply(width, height){
+    return width * height;
+} 
+const area = multiply(width, height);
+console.log(`Area of the pryamokutnyk is ${area}`); */
+
+//Function Expression
+/* const area = function (width, height) {
+    return width * height;
+}
+console.log(area(10,20)); */
---
+++ lesson5/task2.js
@@ -0,0 +1,8 @@
+/* Створіть функцію checkOrder, яка приймає два параметри:
+available (кількість товарів на складі)
+ordered (кількість товарів у замовленні)
+Функція повертає:
+"Your order is too large, we don’t have enough goods." — якщо ordered більше available
+"Your order is empty" — якщо ordered дорівнює 0
+"Your order is accepted" — якщо товару достатньо
+Викличте функцію з різними значеннями available та ordered, виведіть результат у консоль. */
