bb36467f60836fb62e8ad12875d024cca8cc5b0b
Mark <zerofcksgiver@gmail.com>
2026-02-05 03:51:51 +0200

    lesson8 initial commit

---
+++ lesson8/task1.js
@@ -0,0 +1,11 @@
+// Створіть об'єкт calculator, який містить:
+// Властивості:
+// lastResult – останній обчислений результат (початкове значення null).
+// operationsCount – лічильник виконаних операцій (початкове значення 0).
+// Методи:
+// add(a, b): повертає суму a + b та оновлює lastResult і operationsCount.
+// subtract(a, b): повертає різницю a - b та оновлює lastResult і operationsCount.
+// multiply(a, b): повертає добуток a * b та оновлює lastResult і operationsCount.
+// divide(a, b): повертає результат a / b, якщо b !== 0, інакше "Помилка: ділення на нуль!". Також оновлює lastResult і operationsCount.
+// reset(): скидає lastResult до null і operationsCount до 0.
+// Використовуйте ці методи для виконання арифметичних операцій, виведіть результати у консоль та перевірте значення lastResult і operationsCount.
