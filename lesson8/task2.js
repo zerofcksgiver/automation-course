a2a099a57573d4d5ea14d0d4cea43e7c1b434b98
Mark <zerofcksgiver@gmail.com>
2026-02-07 02:03:21 +0200

    lesson8 hw done

--- lesson8/task2.js
+++ lesson8/task2.js
@@ -1,3 +1,8 @@
 // Створіть масив colors, що містить значення: [*список кольорів*].
 // Використовуйте деструктуризацію для отримання перших двох кольорів у змінні color1 і color2.
 // Виведіть їх у консоль.
+
+const colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+const [color1, color2] = colors;
+console.log(color1);
+console.log(color2);
--- lesson8/task3.js
+++ lesson8/task3.js
@@ -5,3 +5,12 @@
 // Додайте до об'єкта властивість email.
 // Видаліть властивість age.
 // Виведіть оновлений об'єкт у консоль.
+
+const person = {
+    firstName: 'echo',
+    lastName: 'Doe',
+    age: 25,
+}
+person.email = 'echo@email.com';
+delete person.age;
+console.log(person); 
